94648f6e3f33839dbe3f54e1eb1733f34072bdf1
dtmiranda <hortadito2@gmail.com>
2023-11-19 15:30:08 -0100

    change main package

--- src/main/java/dev/dtmiranda/moviesManagement/MoviesApplication.java
+++ src/main/java/dev/dtmiranda/moviesManagement/MoviesApplication.java
@@ -1,4 +1,4 @@
-package dev.dtmiranda.movies;
+package dev.dtmiranda.moviesManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/dev/dtmiranda/moviesManagement/MoviesApplicationTests.java
+++ src/test/java/dev/dtmiranda/moviesManagement/MoviesApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.dtmiranda.movies;
+package dev.dtmiranda.moviesManagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
