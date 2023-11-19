e1b13403986149863b5e7524cd2d3538f897ed48
dtmiranda <hortadito2@gmail.com>
2023-11-19 15:31:33 -0100

    create movie class

---
+++ src/main/java/dev/dtmiranda/moviesManagement/movie/Movie.java
@@ -0,0 +1,29 @@
+package dev.dtmiranda.moviesManagement.movie;
+
+import dev.dtmiranda.moviesManagement.Reviews;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data //Take care of Getters an Setters
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+  @Id
+  private Object id;
+  private String imdbId;
+  private String title;
+  private String releaseDate;
+  private String trailerLink;
+  private String Poster;
+  private List<String> genres;
+  private List <String> backdrops;
+  private List<Reviews> reviewIds;
+
+
+}
